233d61bda9eecec7f76846959ccb62ca4a0d517d
mercenarynetwork <venturecapitalarts@gmail.com>
2017-05-09 15:41:37 -0400

    Update matt-plugin.php

--- matt-plugin.php
+++ matt-plugin.php
@@ -1,9 +1,9 @@
 <?php
    /*
-   Plugin Name: Matt's Plugin 1.2
+   Plugin Name: Matt's Plugin 1.3
    Plugin URI: http://fahmah.com
-   Description: An updated plugin for testing the Wp-Pusher thing
-   Version: 1.2
+   Description: Trying to make a shortcode to display the sidebar
+   Version: 1.3
    Author: Matt Jackson
    Author URI: http://fahmah.com
    Date: May 8th 2017
