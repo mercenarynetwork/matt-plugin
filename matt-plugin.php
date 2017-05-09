c7073d555ac598af8b127c786ad885bd85b25927
mercenarynetwork <venturecapitalarts@gmail.com>
2017-05-09 15:50:27 -0400

    Update matt-plugin.php

--- matt-plugin.php
+++ matt-plugin.php
@@ -1,9 +1,9 @@
 <?php
    /*
-   Plugin Name: Matt's Plugin 1.3
+   Plugin Name: Matt's Plugin 1.4
    Plugin URI: http://fahmah.com
    Description: Trying to make a shortcode to display the sidebar
-   Version: 1.3
+   Version: 1.4
    Author: Matt Jackson
    Author URI: http://fahmah.com
    Date: May 8th 2017
@@ -14,8 +14,8 @@
 //[foobar]
 function sidebar_func( $atts ){
 	echo dynamic_sidebar( 'sidebar-1' ); 
-	//return dynamic_sidebar( 'sidebar-1' ); 
-	//return "This is displaying the text but not the sidebar. Damn...";
+	return dynamic_sidebar( 'sidebar-1' ); 
+	return "This is displaying the text but not the sidebar. Damn...";
 }
 add_shortcode( 'sidebar_shorty', 'sidebar_func' );
 
